79ef7352331f761bc8a26a2711634d2996ac5751
Ean Daus <eandaus123@gmail.com>
2019-01-07 14:28:37 -0500

    Added index page with fat free

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Ean Daus
+1/7/19
+index.php
+Fat free/MVC practice project
+*/
+
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload
+require_once 'vendor/autoload.php';
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    echo '<h1>Hello world!</h1>';
+});
+
+//run fat free
+$f3->run();
